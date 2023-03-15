7bbfd2b652fdd48abf28d2277c16cb912d42d934
sirius.zhang <982813440@qq.com>
2023-03-15 17:54:26 +0800

    shadow test

--- src/views/scenes/ShadowTest.vue
+++ src/views/scenes/ShadowTest.vue
@@ -11,10 +11,16 @@ import {
   NearestFilter,
   DoubleSide,
   PlaneGeometry,
-  PCFSoftShadowMap,
+  BasicShadowMap,
+  CameraHelper,
 } from "three";
+import { ShadowMapViewer } from "three/examples/jsm/utils/ShadowMapViewer";
 import { adjustRender, commonRender } from "@/utils/3dtools";
+import { useGui } from "@/hooks/gui";
 const canvas = ref(null as null | HTMLCanvasElement);
+const gui = useGui();
+const guiObj = {};
+
 onMounted(async () => {
   const { renderer, scene, camera } = commonRender(
     canvas.value as HTMLCanvasElement
@@ -23,14 +29,17 @@ onMounted(async () => {
   camera.position.z = 10;
   camera.lookAt(0, 0, 0);
   const ambientLight = new AmbientLight(0xffffff, 0.3);
-  const directionalLight = new DirectionalLight(0xffffff, 0.9);
-  directionalLight.position.y = 10;
+  const directionalLight = new DirectionalLight(0xffffff, 1);
+  directionalLight.position.y = 3;
   directionalLight.position.x = -5;
   const { shadowMap } = renderer;
   shadowMap.enabled = true;
-  shadowMap.type = PCFSoftShadowMap;
   directionalLight.castShadow = true;
   scene.add(ambientLight, directionalLight);
+
+  const shadowMapViewer = new ShadowMapViewer(directionalLight);
+  shadowMapViewer.size.set(128, 128);
+  shadowMapViewer.position.set(10, 10);
 
   const textureLoader = new TextureLoader();
   const floorTexture = await textureLoader.loadAsync("checker.png");
@@ -50,7 +59,8 @@ onMounted(async () => {
   floor.rotation.x = -0.5 * Math.PI;
   floor.receiveShadow = true;
   scene.add(floor);
-  const geometry = new BoxGeometry();
+  const boxSize = 2;
+  const geometry = new BoxGeometry(boxSize, boxSize, boxSize);
   const material = new MeshPhongMaterial({
     color: 0x0dbc79,
   });
@@ -58,11 +68,46 @@ onMounted(async () => {
   cube.position.setY(1);
   cube.castShadow = true;
   scene.add(cube);
+  renderer.shadowMap.type = BasicShadowMap;
+
+  const cameraHelper = new CameraHelper(directionalLight.shadow.camera);
+  scene.add(cameraHelper);
   function animate(time: number) {
+    shadowMapViewer.render(renderer);
     cube.rotation.x = time / 2;
     cube.rotation.y = time / 2;
   }
-  adjustRender(renderer, scene, camera, { beforeRender: animate });
+  adjustRender(renderer, scene, camera, { afterRender: animate });
+  const { shadow } = directionalLight;
+  shadow.mapSize.setScalar(256);
+
+  // add gui
+  gui.add(shadow, "autoUpdate");
+  gui.add(shadow, "bias");
+  gui.add(floor, "castShadow").name("floor castShadow");
+  const light = gui.addFolder("light");
+  [
+    light.add(directionalLight.position, "x"),
+    light.add(directionalLight.position, "y"),
+    light.add(directionalLight.position, "z"),
+    light.add(shadow.camera, "left"),
+    light.add(shadow.camera, "right"),
+    light.add(shadow.camera, "top"),
+    light.add(shadow.camera, "bottom"),
+  ].forEach((v) => {
+    v.onChange(updateCamera);
+  });
+  function updateCamera() {
+    // update the light target's matrixWorld because it's needed by the helper
+    directionalLight.target.updateMatrixWorld();
+    cameraHelper.update();
+    // update the light's shadow camera's projection matrix
+    directionalLight.shadow.camera.updateProjectionMatrix();
+    // and now update the camera helper we're using to show the light's shadow camera
+    cameraHelper.update();
+  }
+  Reflect.set(guiObj, "autoRotate", true);
+  Reflect.set(guiObj, "mapSize", shadow.mapSize.x);
 });
 </script>
 
